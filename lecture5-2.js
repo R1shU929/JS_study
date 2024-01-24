6959423a76a50fbdeaf314e8c7aa350fccabbce3
R1shU929 <charlotte0209@naver.com>
2024-01-24 23:57:46 +0900

    js

--- lecture5-2.js
+++ lecture5-2.js
@@ -37,3 +37,39 @@ console.log(chain0.introEng());
 // -> chain0은 이전에 만들어진 것이지만 수정되어 영어로 출력
 console.log(new YalcoChicken("강남", 17).introEng());
 // 뿐만 아니라 새로 만들 인스턴스도 영문프로토타입 기능 가짐
+
+// 2. 타 방식과의 차이 (솔직히 이해 안됨)
+function YalcoChicken(name, no) {
+  this.name = name;
+  this.no = no;
+  this.introduce = function () {
+    return `안녕하세요, ${this.no}호 ${this.name}점입니다!`;
+  };
+}
+
+function createYalcoChicken(name, no) {
+  return {
+    name,
+    no,
+    introduce() {
+      return `안녕하세요, ${this.no}호 ${this.name}점입니다!`;
+    },
+  };
+}
+// 객체 리터럴
+const chain4 = {
+  name: "판교",
+  no: 3,
+  introduce: function () {
+    return `안녕하세요, ${this.no}호 ${this.name}점입니다!`;
+  },
+};
+
+// 객체 반환 함수
+const chain5 = createYalcoChicken("강남", 17);
+
+// 생성자 함수
+const chain6 = new YalcoChicken("제주", 24);
+console.log(chain4, chain4 instanceof YalcoChicken);
+console.log(chain5, chain5 instanceof YalcoChicken);
+console.log(chain6, chain6 instanceof YalcoChicken);
