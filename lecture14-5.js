c175d0e770e0ce4b341497413d2e849ef0b962bd
R1shU929 <charlotte0209@naver.com>
2024-03-28 14:31:30 +0900

    js study

--- lecture14-5.js
+++ lecture14-5.js
@@ -10,5 +10,5 @@ fetch("https://showcases.yalco.kr/javascript/mockserver/race-result")
   .then((response) => response.json())
   .then(console.log);
 // 반환되는 결과 ( response )
-// 요청의 결과에 대한 정보들을 담은 객체
+// 요청의 결과에 대한 정보들을 담은 객체 !
 // json 메서드 - 결과의 body로 받은 텍스트를 JSON 객체로 변환하여 반환
